118c3a8cad00b000aa1863238de28b0ec3e96dd8
Clayton Rieck <cjrieck123@gmail.com>
2016-06-06 12:05:17 -0400

    updated window settings

--- src/main.mm
+++ src/main.mm
@@ -2,10 +2,16 @@
 #include "ofApp.h"
 
 int main(){
-    ofGLWindowSettings s;
-    s.setGLVersion(3,2);
-    ofCreateWindow(s);
+    ofiOSWindowSettings settings;
+    settings.enableRetina = false; // enables retina resolution if the device supports it.
+    settings.enableDepth = false; // enables depth buffer for 3d drawing.
+    settings.enableAntiAliasing = false; // enables anti-aliasing which smooths out graphics on the screen.
+    settings.numOfAntiAliasingSamples = 0; // number of samples used for anti-aliasing.
+    settings.enableHardwareOrientation = false; // enables native view orientation.
+    settings.enableHardwareOrientationAnimation = false; // enables native orientation changes to be animated.
+    settings.glesVersion = OFXIOS_RENDERER_ES1; // type of renderer to use, ES1, ES2, ES3
+    settings.windowMode = OF_FULLSCREEN;
+    ofCreateWindow(settings);
     
-	ofSetupOpenGL(1024,720,OF_FULLSCREEN);			// <-------- setup the GL context
-    ofRunApp(new ofApp());
+    return ofRunApp(new ofApp);
 }
